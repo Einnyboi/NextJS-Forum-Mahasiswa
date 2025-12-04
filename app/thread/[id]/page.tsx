89e7f296ace0b6315269237351c66d3dc6083ada
nomii1568 <naomi.sugiantara@gmail.com>
2025-12-05 00:21:33 +0700

    merging main

--- app/thread/[id]/page.tsx
+++ app/thread/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import Navbar from "@/components/features/important/navbar";
 import Sidebar from "@/components/features/important/sidebar";
 import { ThreadCard } from "@/components/features/thread/ThreadCard";
